b0a11f2b73412c4f6e0d6bddc6f1a045016f84e9
harpsaini <harpreetsinghsn@gmail.com>
2021-10-19 16:58:03 -0400

    included form

--- index.js
+++ index.js
@@ -2,19 +2,21 @@
 
 //makes nav bar fixed and add background colour
 const fixedNav = document.querySelector("header nav")
+const navBtn = document.querySelectorAll(".navButton")
+const buttonToShowForm = document.querySelector('.contactMe a')
+const form = document.querySelector('form')
+console.log(form);
 
 window.addEventListener('scroll', ()=>{
   let scrolledDistance = window.scrollY;
-
-  console.log('scrolledDistance');
   if(scrolledDistance > 10){
     fixedNav.classList.add("scrolled")
    }
   else{
     fixedNav.classList.remove("scrolled")
-  }
+  } 
 })
 
-
-
-
+buttonToShowForm.addEventListener('click',()=>{
+  form.classList.remove("hiddenForm")
+})
