8c579224c258f47be6b506f56139b2502ac60de7
harpsaini <harpreetsinghsn@gmail.com>
2021-10-19 08:52:13 -0400

    adding animation

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello world")
+
+
+//makes nav bar fixed and add background colour
+const fixedNav = document.querySelector("header nav")
+
+window.addEventListener('scroll', ()=>{
+  if(scroll){
+    fixedNav.classList.add("scrolled")
+  }
+  // else{
+  //   fixedNav.classList.remove("scrolled")
+  // }
+})
+
+
