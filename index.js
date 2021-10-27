b04585718c0288175d4f6d3777b2943f0e078ee4
harpsaini <harpreetsinghsn@gmail.com>
2021-10-26 23:03:40 -0400

    included particles.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+
 
 
 //makes nav bar fixed and add background colour
